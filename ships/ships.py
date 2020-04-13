966c1d353301fb4d905df965b63e1ebe5a8b7b27
Levi Cowan <meridionaljet@gmail.com>
2020-04-13 12:57:39 -0400

    convert some parameter units; allow get_storm_obs() to select a specific time

--- ships/ships.py
+++ ships/ships.py
@@ -2,6 +2,7 @@ __all__ = ['Ships']
 
 from datetime import datetime
 import logging
+import numpy as np
 import os, sys
 from pprint import pprint
 import sqlite3
@@ -21,11 +22,15 @@ sys.excepthook = exc_hook
 
 
 class Ships:
+
+    # Raw file missing value
+    MISSING = 9999
+
     def __init__(self):
         self.logfile = logfile
         self.datadir = os.path.join(workdir, 'data')
         self.rawtext_filename = os.path.join(self.datadir, 'ships.txt')
-        self.documentation_filename = os.path.join(self.datadir, 'ships_predictor_file_2020.txt')
+        self.doc_filename = os.path.join(self.datadir, 'ships_predictor_file_2020.txt')
         if not os.path.exists(self.datadir):
             os.makedirs(self.datadir, 0o755)
         self.db_filename = os.path.join(self.datadir, 'ships.db')
@@ -38,7 +43,7 @@ class Ships:
         {parameter_name: description}
         """
         self.parameter_descriptions = {}
-        with open(self.documentation_filename) as f:
+        with open(self.doc_filename) as f:
             for line in f.readlines():
                 parts = [p.strip() for p in line.split(':')]
                 param = parts[0]
@@ -70,7 +75,12 @@ class Ships:
         # Create diagnostics table. If it already exists, replace it.
         c = self.db.cursor()
         c.execute(f'DROP TABLE IF EXISTS {self.tablename}')
-        blacklist = ['TIME'] # Some parameters we don't want or are redundant
+        # Some parameters we don't want, or are not actually defined as time-dependent
+        # (e.g., satellite-derived parameters and TPW distribution, which are an array
+        # of values for one time, and not easily stored in the database the same way)
+        blacklist = ['TIME', 'MTPW', 'IRXX', 'IR00', 'IRM1', 'IRM3', 'PC00', 'PCM1', 'PCM3'
+                     'PSLV', ]
+        blacklist += [f'PW{x:02}' for x in range(0, 20)]
         colnames = [name for name in self.get_diag_names() if name not in blacklist]
         # Table has a couple identifying columns, followed by all diagnostic parameters
         # stored as integers (as in the raw file)
@@ -124,7 +134,7 @@ class Ships:
                 linenum += 1
         print()
 
-    def get_storm_obs(self, ATCF_ID):
+    def get_storm_obs(self, ATCF_ID, time=None):
         """
         Fetch all SHIPS obs for the given storm from the SQL database.
         This only works for storms with an assigned ATCF ID (Atlantic, EPAC, CPAC storms)
@@ -132,114 +142,57 @@ class Ships:
         Args:
             ATCF_ID: e.g., 'AL132005'
 
+            time:    (datetime or None) If provided, only data from the requested time is returned.
+                                        If None (default), data for all times is returned.
+
         Returns:
-            A dict of form {parameter: [list of time-ordered values]}
+            A dict of form {parameter: values}. If `time` is None, values is an array. If `time`
+            is specified, values is a single value.
         """
-        rows = list(self.db.execute(f'SELECT * FROM {self.tablename} WHERE ATCF_ID="{ATCF_ID}" ORDER BY TIME'))
+        if time is None:
+            query = f'SELECT * FROM {self.tablename} WHERE ATCF_ID="{ATCF_ID}" ORDER BY TIME'
+        else:
+            t = time.strftime('%Y-%m-%d %H:%M:%S')
+            query = f'SELECT * FROM {self.tablename} WHERE ATCF_ID="{ATCF_ID}" AND TIME="{t}"'
+        rows = list(self.db.execute(query))
         colnames = [info[1] for info in self.db.execute(f'PRAGMA table_info("{self.tablename}")')]
         values = list(zip(*rows))
-        data = {colname: values for colname, values in zip(colnames, values)}
+        dtypes = {'ATCF_ID': 'U8', 'TIME': 'datetime64[m]'}
+        data = {}
+        for colname, vals in zip(colnames, values):
+            # All but a couple parameters will be numerical - use float so we can set missing to NaN
+            dtype = dtypes.get(colname, float)
+            # Time needs special reformatting
+            if colname == 'TIME':
+                vals = [datetime.strptime(s, '%Y-%m-%d %H:%M:%S') for s in vals]
+            arr = np.array(vals, dtype=dtype)
+            if dtype is float:
+                arr[np.isclose(arr, self.MISSING)] = np.nan
+            # Whether we store an array or a single value depends on whether a time was specified
+            if time is not None and arr.size:
+                data[colname] = arr[0]
+            else:
+                data[colname] = arr
+        data = self._convert_units(data)
         return data
 
-    def load_from_db(self):
-        """Parse SQL database and (re)construct Storm objects"""
-        logger.info('Loading all TCs from database...')
-        self.storms.clear()
-        rows = list(self.db.execute(f'SELECT * FROM {self.tablename} ORDER BY ID,time'))
-        # Group rows by TC ID
-        rows_by_TC = {}
-        for row in rows:
-            rows_by_TC.setdefault(row[0], []).append(row)
-        # Format into mapping of column names to column data
-        colnames = [info[1] for info in self.db.execute(f'PRAGMA table_info("{self.tablename}")')]
-        for storm_rows in rows_by_TC.values():
-            values = list(zip(*storm_rows))
-            data = {colname: values for colname, values in zip(colnames, values)}
-            self.storms.append(Storm(data, datatype='db'))
-        self.resolve_duplicates()
-
-    def load_all_storms(self, source='db'):
+    def _convert_units(self, data):
         """
-        Load all Storm objects into self.storms
+        Convert some commonly-used diagnostics (not all of them) to their standard units.
+        These changes and the encoded units of all other parameters are reflected in
+        self.doc_filename
 
         Args:
-            source: If 'db': load from SQL database generated by self.save_to_db()
-                    If 'json': load from JSON files generated by self.save_to_json()
-                    If 'csv': load from the raw IBTrACS CSV file
-        """
-        if source == 'db':
-            assert os.path.exists(self.db_filename), 'database file does not exist'
-            self.load_from_db()
-        elif source == 'json':
-            assert os.path.exists(os.path.join(workdir, 'data/json')), 'JSON files do not exist'
-            self.load_from_json()
-        elif source == 'csv':
-            assert os.path.exists(os.path.join(self.datadir, 'ibtracs.csv'))
-            self.load_from_csv()
-        else:
-            raise ValueError(f'Unrecognized source: {source}')
-        # Sort storm list by genesis time
-        self.storms.sort(key=lambda tc: tc.genesis)
+            data: dict of form {param_name: 1D array}
 
-    def save_to_db(self):
+        Returns:
+            Same data structure as input
         """
-        Save all storm objects to an sqlite3 database
-        """
-        if not self.storms:
-            logger.info('Parsing storm data...')
-            self.load_all_storms(source='csv')
-        c = self.db.cursor()
-        # Create storm table. If it already exists, replace it.
-        c.execute(f'DROP TABLE IF EXISTS {self.tablename}')
-        c.execute(f"""
-            CREATE TABLE {self.tablename}(
-                ID CHAR(13),      ATCF_ID CHAR(8),
-                name VARCHAR,     season INT,
-                basin CHAR(2),    subbasin CHAR(2),
-                lat FLOAT,        lon FLOAT,
-                time DATETIME,    wind INT,
-                mslp INT,         classification CHAR(2),
-                speed FLOAT,      dist2land INT,
-                genesis DATETIME, agency VARCHAR,
-                R34_SE FLOAT,     R34_NE FLOAT,
-                R34_SW FLOAT,     R34_NW FLOAT,
-                R50_SE FLOAT,     R50_NE FLOAT,
-                R50_SW FLOAT,     R50_NW FLOAT,
-                R64_SE FLOAT,     R64_NE FLOAT,
-                R64_SW FLOAT,     R64_NW FLOAT
-        )""")
-        self.db.commit()
-
-        # Insert each track point as a row
-        rows = []
-        radii_attrs = [f'R{v}_{q}' for v in (34,50,64) for q in ('NE','SE','SW','NW')]
-        for tc in self.storms:
-            genesis = tc.genesis.strftime('%Y-%m-%d %H:%M:%S')
-            for i in range(len(tc.time)):
-                t = tc.time[i].item().strftime('%Y-%m-%d %H:%M:%S')
-                vals = (
-                    tc.ID, tc.ATCF_ID, tc.name, tc.season, tc.basins[i], tc.subbasins[i],
-                    tc.lat[i], tc.lon[i], t, tc.wind[i], tc.mslp[i],
-                    tc.classification[i], tc.speed[i], tc.dist2land[i], genesis, tc.agencies[i]
-                )
-                # Wind radii values
-                rvals = tuple(getattr(tc, attr)[i] for attr in radii_attrs)
-                row = vals + rvals
-                rows.append(row)
-        logger.info(f'Inserting {len(rows)} rows into database...')
-        c.executemany(f'INSERT INTO {self.tablename} VALUES ({",".join("?"*len(rows[0]))})', rows)
-        self.db.commit()
-
-
-    def get_storm(self, name, season, basin):
-        """
-        Fetch a TC from the SQL database and construct a Storm object.
-        This can only be used to get storms with a defined name
-        """
-        rows = list(self.db.execute(f'SELECT * FROM {self.tablename} WHERE name = "{name.upper()}" AND season = {season} AND basin = "{basin}" ORDER BY time'))
-        if not rows:
-            raise ValueError(f'Storm not found in database: name={name}, season={season}, basin={basin}')
-        colnames = [info[1] for info in self.db.execute(f'PRAGMA table_info("{self.tablename}")')]
-        values = list(zip(*rows))
-        data = {colname: values for colname, values in zip(colnames, values)}
-        return Storm(data, datatype='db')
+        conversion_factors = {
+            'LAT': 0.1, 'LON': 0.1, 'CSST': 0.1, 'RSST': 0.1, 'DSST': 0.1, 'DSTA': 0.1,
+            'NSST': 0.1, 'XDST': 0.1, 'U200': 0.1, 'T150': 0.1, 'T200': 0.1, 'T250': 0.1,
+            'SHRD': 0.1, 'SHRS': 0.1,
+        }
+        for param, fac in conversion_factors.items():
+            data[param] *= fac
+        return data
